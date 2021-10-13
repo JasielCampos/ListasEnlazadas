6de23b663e29ba84f7eb4f934a7d670bb6f26dff
Jasiel Campos Romero <jasielcamposromero@hotmail.com>
2021-10-13 15:03:03 -0400

    Agregar archivos de proyecto.

---
+++ ListasEnlazadas/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace ListasEnlazadas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Insertar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(452, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(31, 166);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(606, 124);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ ListasEnlazadas/ListaSimplementeEnlazada.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListasEnlazadas
+{
+    class ListaSimplementeEnlazada
+    {
+        public Nodo tope = null;
+
+        private bool VaciaPila()
+        {
+            if (tope == null)
+                return true;
+            else
+                return false;
+        }
+
+        public void InsertarPila(int elemento)
+        {
+            Nodo p;
+            if (VaciaPila())
+            {
+                tope = new Nodo();
+                tope.Informacion = elemento;
+                tope.Enlace = null;
+            }
+            else
+            {
+                p = new Nodo();
+                p.Informacion = elemento;
+                p.Enlace = tope;
+                tope = p;
+            }
+        }
+
+        public void EliminarPila()
+        {
+            Nodo p;
+            if (VaciaPila())
+            {
+                MessageBox.Show("La pila esta vacia, no se puede eliminar elementos");
+            }
+            else
+            {
+                MessageBox.Show("El elemento eliminado es: "+tope.Informacion);
+                p = tope;
+                tope = tope.Enlace;
+                p.Enlace = null;
+                p = null;
+            }
+        }
+    }
+}
